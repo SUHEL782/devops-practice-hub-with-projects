25c0410a5e5fcc8c7ea65ef2459a008b07f530d8
suhelkhan <thecomputersk@gmail.com>
2026-04-04 16:02:43 +0530

    fix

--- DevOps Project-02/modules/aws-rds/variables.tf
+++ DevOps Project-02/modules/aws-rds/variables.tf
@@ -1,6 +1,6 @@
 variable "sg-name" {}
 variable "private-subnet-name1" {}
-variable "private-subnet-name2" {}
+variable "private-subnet-name2" {}  # pls ensure all proper configure
 variable "db-sg-name" {}
 variable "rds-username" {}
 variable "rds-pwd" {}
